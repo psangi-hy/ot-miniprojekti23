6e5c84f8cd07f969c38cba4cded91856760af6ac
Pyry Sangi <pyry.sangi@helsinki.fi>
2023-11-22 13:43:36 +0200

    Clean up comparison with True

--- src/db.py
+++ src/db.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from app import app
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-if app.testing == True:
+if app.testing:
     app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(
             basedir, 'data/test_database.sqlite')
 else:
