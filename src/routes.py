1ea18bdf551b2f9f95d3c1b7f5cfbfd0c8d6873b
smausquared <samu.ayhynmaki@gmail.com>
2023-11-22 12:35:11 +0200

    route for new article

--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,24 @@
 from app import app
 import db_handling
-from flask import render_template, redirect, session, request
+from flask import render_template, redirect, request
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     items = db_handling.database_test()
     return render_template("index.html", items=items)
+
+
+@app.route("/new", methods=["GET", "POST"])
+def new():
+    if request.method == "GET":
+        return render_template("new.html")
+    elif request.method == "POST":
+        key = request.form("key")
+        author = request.form("author")
+        title = request.form("title")
+        journal = request.form("journal")
+        year = request.form("year")
+        volume = request.form("volume")
+        pages = request.form("pages")
+        db_handing.new_article(key, author, title, journal, year, volume, pages)
+        return redirect("/")
