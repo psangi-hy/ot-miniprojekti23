7678c0089e5c7d3c53c1fdd008c78d85b585dd6a
lpatrik <lpatrik@ad.helsinki.fi>
2023-11-22 12:36:04 +0200

    Addded new article functoin to db_handling

--- src/db_handling.py
+++ src/db_handling.py
@@ -1,7 +1,12 @@
 from db import db
 from sqlalchemy.sql import text
 
-def database_test():
+def select_all_articles():
     sql = text("SELECT * FROM articles")
     result = db.session.execute(sql)
     return result
+
+def new_article(key, author, title, journal, year, volume, pages):
+    sql = text("INSERT INTO articles (key, author, title, journal, year, volume, pages) VALUES (:key, :author, :title, :journal, :year, :volume, :pages)")
+    db.session.execute(sql, {"key": key, "author": author, "title": title, "journal": journal, "year": year, "volume": volume, "pages": pages})
+    db.session.commit()
