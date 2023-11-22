2487d91cb3a0f17ddeca913261e4a5ab717a8f32
Pyry Sangi <pyry.sangi@helsinki.fi>
2023-11-22 13:32:10 +0200

    Clean up line lengths

--- src/db.py
+++ src/db.py
@@ -4,7 +4,9 @@ from app import app
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 if app.testing == True:
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'data/test_database.sqlite')
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(
+            basedir, 'data/test_database.sqlite')
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'data/database.sqlite')
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(
+            basedir, 'data/database.sqlite')
 db = SQLAlchemy(app)
--- src/db_handling.py
+++ src/db_handling.py
@@ -8,6 +8,14 @@ def select_all_articles():
     return result
 
 def new_article(key, author, title, journal, year, volume, pages):
-    sql = text("INSERT INTO articles (key, author, title, journal, year, volume, pages) VALUES (:key, :author, :title, :journal, :year, :volume, :pages)")
-    db.session.execute(sql, {"key": key, "author": author, "title": title, "journal": journal, "year": year, "volume": volume, "pages": pages})
+    sql = text(
+            "INSERT INTO articles (key, author, title, journal, year, volume, pages)"
+            "VALUES (:key, :author, :title, :journal, :year, :volume, :pages)")
+    db.session.execute(sql, {"key": key,
+                             "author": author,
+                             "title": title,
+                             "journal": journal,
+                             "year": year,
+                             "volume": volume,
+                             "pages": pages})
     db.session.commit()
