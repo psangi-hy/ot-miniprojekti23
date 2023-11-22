26bbb3cc22239b99bbdacc47d192919fe15c1a76
smausquared <samu.ayhynmaki@gmail.com>
2023-11-22 12:44:22 +0200

    fix form request

--- src/routes.py
+++ src/routes.py
@@ -13,12 +13,12 @@ def new():
     if request.method == "GET":
         return render_template("new.html")
     elif request.method == "POST":
-        key = request.form("key")
-        author = request.form("author")
-        title = request.form("title")
-        journal = request.form("journal")
-        year = request.form("year")
-        volume = request.form("volume")
-        pages = request.form("pages")
+        key = request.form["key"]
+        author = request.form["author"]
+        title = request.form["title"]
+        journal = request.form["journal"]
+        year = request.form["year"]
+        volume = request.form["volume"]
+        pages = request.form["pages"]
         db_handling.new_article(key, author, title, journal, year, volume, pages)
         return redirect("/")
