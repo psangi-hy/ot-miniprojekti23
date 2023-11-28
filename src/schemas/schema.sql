a7c5c866de50de79dc2bd85c330428301e6b9be4
smausquared <samu.ayhynmaki@gmail.com>
2023-11-28 12:54:37 +0200

    add book inproceedings to schema

--- src/schemas/schema.sql
+++ src/schemas/schema.sql
@@ -8,3 +8,24 @@ CREATE TABLE articles(
     volume VARCHAR (100),
     pages VARCHAR (50)
 );
+
+CREATE TABLE books(
+    id INTEGER PRIMARY KEY,
+    key VARCHAR (40) UNIQUE,
+    author VARCHAR (100),
+    title VARCHAR (200),
+    year INTEGER,
+    publisher VARCHAR (100),
+    volume VARCHAR (100),
+    pages VARCHAR (50)
+);
+
+CREATE TABLE inproceedings(
+    id INTEGER PRIMARY KEY,
+    key VARCHAR (40) UNIQUE,
+    author VARCHAR (100),
+    title VARCHAR (200),
+    year INTEGER,
+    booktitle VARCHAR (100),
+    pages VARCHAR (50)
+);
