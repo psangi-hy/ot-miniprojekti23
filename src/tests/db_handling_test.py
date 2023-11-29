1f95d00c26bc6c697a6ce6c2fefe2790858bf647
jnnhan <jenna.hanski@helsinki.fi>
2023-11-29 17:29:40 +0200

    unittests test functions for adding new books and inproceedings

--- src/tests/db_handling_test.py
+++ src/tests/db_handling_test.py
@@ -72,38 +72,37 @@ class Testdbhandling(unittest.TestCase):
         self.app_context.pop()
     
     def test_new_book(self):
-        result = db_handling.new_article("key1", "author1", "title1", "journal1", 2022, "1", "10-15")
+        result = db_handling.new_book("key1", "author1", "title1", 2022, "publisher1", "1", "10-15")
         self.assertTrue(result)
 
-        articles = db_handling.select_all_articles()
-        self.assertEqual(len(articles), 1)
+        books = db_handling.select_all_books()
+        self.assertEqual(len(books), 1)
 
-        article = articles[0]
-        self.assertEqual(article.key, "key1")
-        self.assertEqual(article.author, "author1")
-        self.assertEqual(article.title, "title1")
-        self.assertEqual(article.journal, "journal1")
-        self.assertEqual(article.year, 2022)
-        self.assertEqual(article.volume, "1")
-        self.assertEqual(article.pages, "10-15")
+        book = books[0]
+        self.assertEqual(book.key, "key1")
+        self.assertEqual(book.author, "author1")
+        self.assertEqual(book.title, "title1")
+        self.assertEqual(book.year, 2022)
+        self.assertEqual(book.publisher, "publisher1")
+        self.assertEqual(book.volume, "1")
+        self.assertEqual(book.pages, "10-15")
 
         self.app_context.pop()
     
     def test_new_inproceeding(self):
-        result = db_handling.new_article("key1", "author1", "title1", "journal1", 2022, "1", "10-15")
+        result = db_handling.new_inproceeding("key1", "author1", "title1", 2022, "booktitle1", "10-15")
         self.assertTrue(result)
 
-        articles = db_handling.select_all_articles()
-        self.assertEqual(len(articles), 1)
+        inproceedings = db_handling.select_all_inproceedings()
+        self.assertEqual(len(inproceedings), 1)
 
-        article = articles[0]
-        self.assertEqual(article.key, "key1")
-        self.assertEqual(article.author, "author1")
-        self.assertEqual(article.title, "title1")
-        self.assertEqual(article.journal, "journal1")
-        self.assertEqual(article.year, 2022)
-        self.assertEqual(article.volume, "1")
-        self.assertEqual(article.pages, "10-15")
+        inproceeding = inproceedings[0]
+        self.assertEqual(inproceeding.key, "key1")
+        self.assertEqual(inproceeding.author, "author1")
+        self.assertEqual(inproceeding.title, "title1")
+        self.assertEqual(inproceeding.year, 2022)
+        self.assertEqual(inproceeding.booktitle, "booktitle1")
+        self.assertEqual(inproceeding.pages, "10-15")
 
         self.app_context.pop()
     
