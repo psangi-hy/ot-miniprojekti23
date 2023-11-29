627bba9227ccd92104964fbdc8eae9b780a38749
Pyry Sangi <pyry.sangi@helsinki.fi>
2023-11-29 13:38:25 +0200

    Revert routes.py to version that handles bad form
    Small changes to the version in question:
    - Leave in the misspelling of inproceedings, so
      that it doesn't have to be changed everywhere.
    - Appease pylint gods.
    - Whitespace.

--- src/routes.py
+++ src/routes.py
@@ -5,44 +5,43 @@ import db_handling
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    articles = list(db_handling.select_all_articles())
-    books = list(db_handling.select_all_books())
-    inproceedings = list(db_handling.select_all_inproceedings())
-    return render_template(
-        "index.html", articles=articles, books=books, inproceedings=inproceedings)
+    articles = db_handling.select_all_articles()
+    books = db_handling.select_all_books()
+    inproceedings = db_handling.select_all_inproceedings()
+    return render_template("index.html", articles=articles, books=books, inproceedings=inproceedings)
 
 
 @app.route("/new", methods=["GET", "POST"])
 def new():
-    if request.method == "POST":
-        reference_type = request.form.get("type")
-        author = request.form.get("author")
-        title = request.form.get("title")
-        year = request.form.get("year")
-        pages = request.form.get("pages")
+    if request.method == "GET":
+        return render_template("new.html")
 
-        journal_or_publisher_or_booktitle = (request.form.get("journal")
-            or request.form.get("publisher")
-            or request.form.get("booktitle"))
-        volume = request.form.get("volume")
+    reference_type = request.form.get("type", default="")
+    author = request.form.get("author", default="")
+    year = request.form.get("year", default="")
+    volume = request.form.get("volume", default="")
+    title = request.form.get("title", default="")
+    journal = request.form.get("journal", default="")
+    pages = request.form.get("pages", default="")
+    publisher = request.form.get("publisher", default="")
+    booktitle = request.form.get("booktitle", default="")
 
-        # Generate a key for the reference
-        key = db_handling.bibtexgen(author, year, volume, pages)
+    # Luo key käyttäen authorin isoja kirjaimia, julkaisu vuotta, painosta ja sivunumeroita
+    key = db_handling.bibtexgen(author,year,volume,pages)
 
-        if reference_type == "article":
-            if db_handling.new_article(
-                key, author, title, journal_or_publisher_or_booktitle, year, volume, pages):
-                return redirect("/")
-        elif reference_type == "book":
-            if db_handling.new_book(
-                key, author, title, year, journal_or_publisher_or_booktitle, volume, pages):
-                return redirect("/")
-        elif reference_type == "inproceeding":
-            if db_handling.new_inproceeding(
-                key, author, title, year, journal_or_publisher_or_booktitle, pages):
-                return redirect("/")
+    if reference_type == "article" and db_handling.new_article(
+            key, author, title, journal, year, volume, pages):
+        return redirect("/")
 
-    return render_template("new.html")
+    if reference_type == "book" and db_handling.new_book(
+            key, author, title, year, publisher, volume, pages):
+        return redirect("/")
+
+    if reference_type == "inproceeding" and db_handling.new_inproceeding(
+            key, author, title, year, booktitle, pages):
+        return redirect("/")
+
+    return render_template("error.html", message="Something went wrong...")
 
 
 @app.route("/tests/reset", methods=["GET", "POST"])
